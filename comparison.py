4e569b81ca9d416a84a9be5ba8b28c21425bc703
Carsten Pohl <c@rstenpohl.de>
2020-07-22 08:28:34 +0200

    New module for comparing filelists

--- comparison.py
+++ comparison.py
@@ -1,10 +1,23 @@
 
-
+def get_md5_set(filename):
+    md5_list = list()
+    print(f'Reading {filename}.')
+    with open(filename,'r', encoding='utf-8') as file_handle:
+        for line in file_handle.readlines():
+            md5_list.append(line.split(';')[0])    
+    md5_set = set(md5_list)
+    if len(md5_list) != len(md5_set):
+        print(f'There are files with same md5 checksum in {filename}')
+    return md5_set
 
 def are_all_local_files_in_remote():
-    local_md5_values = list()
-    with open('local_filelist.csv','r', encoding='utf-8') as local_file_handle:
-        for line in local_file_handle.readlines():
-            local_md5_values.append(line.split(';')[0])
-    print(local_md5_values)
+    local_md5 = get_md5_set('local_filelist.csv')
+    remote_md5 = get_md5_set('adrive_filelist.csv')
+
+    diff = local_md5 - remote_md5
+    if len(diff) == 0:
+        print('All local files are in remote.')
+    else:
+        print('The difference is:')
+        print(diff)
 
